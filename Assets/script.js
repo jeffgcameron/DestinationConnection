e5eecc16c261c45a594216fbd81c73bd3536885c
Jeff Cameron <jeffgcameron@gmail.com>
2021-08-13 13:04:43 -0600

    info and contact

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,22 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav')
-    const navLinks = document.querySelectorAll('.nav li');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav')
+//     const navLinks = document.querySelectorAll('.nav li');
 
-    burger.addEventListener('click',() =>{
-        nav.classList.toggle('nav-active');
-    })
-}
+//     burger.addEventListener('click',() =>{
+//         nav.classList.toggle('nav-active');
+//     })
+// }
 
-navSlide();
+// navSlide();
+
+$(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".header").removeClass("active");
+        }
+    });
+});
