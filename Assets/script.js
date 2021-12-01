3d94582d73712098dbcef230b2396bea9fd7d9f4
Jeff Cameron <jeffgcameron@gmail.com>
2021-12-01 16:45:41 -0700

    wedding page

--- Assets/script.js
+++ Assets/script.js
@@ -5,18 +5,33 @@ const arrowLeft = document.getElementById("arrow-left");
 
 const reviews = [
   {
+    content:"Year after year, you continue to make our event a success. You definitely know us and Jackson Hole well (and make us look good)! We honestly could not keep doing this without you and we recognize each and every step you take in making the magic happen! " ,
+
+    creator:"Pedro, March 2018" ,
+  },
+  {
+    content:"Thank you for making [our client’s] incentive program a huge success. If it weren’t for you and your persistence during our initial site visit, our group would not have come to Jackson.  I appreciate you! " ,
+
+    creator:"Kara, February 2017" ,
+  },
+  {
+    content:"We can’t thank you enough for all of the time and effort that you have put into planning our program. You have been great to work with and with you handling so many details, we know it will be a great success!" ,
+
+    creator:"Caroline and Katherine, September 2021" ,
+  },
+  {
     content:
-      "I don’t know where to start saying thank you to you gal! You are truly a joy to work with, your efficiency blew me away.  I’m fairly detail oriented but you beat me by a mile. Thank you a hundred times for all you did to make our program such a success and for being who you are!",
+      "I don’t know where to start saying thank you to you gal! You are truly a joy to work with, your efficiency blew me away. I’m fairly detail oriented but you beat me by a mile. Thank you a hundred times for all you did to make our program such a success and for being who you are!",
     creator: "Cici, August 2008",
   },
   {
     content:
-      "Thank you for all you do behind the scenes to make me look good.  And for going way beyond your job scope to take care of me and our group.",
+      "Thank you for all you do behind the scenes to make me look good. And for going way beyond your job scope to take care of me and our group.",
     creator: "David, January 2007",
   },
   {
     content:
-      "Where can we start to thank you for leading the way for our absolutely perfect program.  You were incredible to work with. . . Knowledgeable, creative, flexible, unfrazzled and always there for us! Thank you for all the guidance you gave us and your patience through the challenging decisions and logistics.  It was a great experience working with you and the result was a A+++.",
+      "Where can we start to thank you for leading the way for our absolutely perfect program. You were incredible to work with. . . Knowledgeable, creative, flexible, unfrazzled and always there for us! Thank you for all the guidance you gave us and your patience through the challenging decisions and logistics.  It was a great experience working with you and the result was a A+++.",
 
     creator: "Irene and Cindy, August 2009",
   },
@@ -46,16 +61,6 @@ const reviews = [
     creator:"Julie, September 2013" ,
   },
   {
-    content:"Thank you for making [our client’s] incentive program a huge success.  If it weren’t for you and your persistence during our initial site visit, our group would not have come to Jackson.  I appreciate you! " ,
-
-    creator:"Kara, February 2017" ,
-  },
-  {
-    content:"We can’t thank you enough for all of the time and effort that you have put into planning our program. You have been great to work with and with you handling so many details, we know it will be a great success!" ,
-
-    creator:"Caroline and Katherine, September 2021" ,
-  },
-  {
     content:"Thank you for all your help again this year.  You make everything very easy! I’m looking forward to working with you again next year and hopefully we’ll finally get to go out and ski together! " ,
 
     creator:" Paula, March 2016" ,
@@ -69,11 +74,6 @@ const reviews = [
     content:"Through your own experience with large groups, you know it is never easy to coordinate game plans and keep everyone happy.  You and your team manage to do just that- - and made it look easy.  You are one of the most unflappable and highly competent individuals I have every had the pleasure to work with.  You deserve a healthy portion of the credit for [international company] booking its unprecedented fifth event at Jackson Hole.  I eagerly anticipate working with you again. " ,
 
     creator:"Cathy, February 2017" ,
-  },
-  {
-    content:"Year after year, you continue to make our event a success.  You definitely know us and Jackson Hole well (and make us look good)! We honestly could not keep doing this without you and we recognize each and every step you take in making the magic happen! " ,
-
-    creator:"Pedro, March 2018" ,
   },
   {
     content:"Our winning combination program was a “winner”!  Every family had a fantastic trip and that wouldn’t have happened with your contributions! " ,
